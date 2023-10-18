5aa4bd869e8ca6e8573398107442dd1e8a44b035
rahul-mysa <119628977+rahul-mysa@users.noreply.github.com>
2023-10-18 20:23:48 -0230

    First working hello world app deployed to JIRA cloud.

---
+++ Jira_App/Issue-Templates/src/index.jsx
@@ -0,0 +1,17 @@
+import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+
+
+const App = () => {
+return (
+<Fragment>
+<Text>Hello world!</Text>
+</Fragment>
+);
+};
+
+
+export const run = render(
+<IssuePanel>
+<App />
+</IssuePanel>
+);
