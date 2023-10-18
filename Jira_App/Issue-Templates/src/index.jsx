8a6e4f112834b050de9a8350deaf307876cacd6d
rahul-mysa <119628977+rahul-mysa@users.noreply.github.com>
2023-10-18 20:29:03 -0230

    Button added to app. On button click, add a text to comment using api. No errors but comment is not saved to the jira issue.

--- Jira_App/Issue-Templates/src/index.jsx
+++ Jira_App/Issue-Templates/src/index.jsx
@@ -1,17 +1,42 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
-
+import ForgeUI, { render, Button, IssuePanel, useProductContext } from '@forge/ui';
+import api, { route } from '@forge/api';
 
 const App = () => {
-return (
-<Fragment>
-<Text>Hello world!</Text>
-</Fragment>
-);
+  const context = useProductContext();
+
+  const handleInsertComment = async () => {
+    if (!context.platformContext) {
+      console.error('platformContext is not available.');
+      return;
+    }
+
+    // Construct the content to be inserted into the comment field.
+    const contentToInsert = 'Test comment for thermoaihackathon';
+
+    try {
+      // Use the Forge issue APIs to update the comment field.
+      await api.asUser().requestJira(route`/rest/api/3/issue/${context.platformContext.issueKey}/comment`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ body: contentToInsert }),
+      });
+
+      console.log('Comment added successfully to issue ' + context.platformContext.issueKey + '.');
+
+    } catch (error) {
+      console.error('Error updating comment field:', error);
+    }
+  };
+
+  return (
+    <Button text="Insert some text into Comment" onClick={handleInsertComment} />
+  );
 };
 
-
 export const run = render(
-<IssuePanel>
-<App />
-</IssuePanel>
+  <IssuePanel>
+    <App />
+  </IssuePanel>
 );
