20d688465cfcab86a43260988300e15de745f1e1
rahul-mysa <119628977+rahul-mysa@users.noreply.github.com>
2023-10-19 15:08:08 -0230

    modal opens on button click. modal shows bug retest form. almost ready for demo

--- Jira_App/Issue-Templates/src/index.jsx
+++ Jira_App/Issue-Templates/src/index.jsx
@@ -12,7 +12,10 @@ import ForgeUI, {
   Text,
   IssuePanel,
   Button,
-  ModalDialog
+  ModalDialog,
+  RadioGroup,
+  Radio,
+  Image
 } from "@forge/ui";
 
 const App = () => {
@@ -32,67 +35,62 @@ const App = () => {
     setFormState(formData);
   };
 
-  const goBack = () => {};
-  const cancel = () => {};
-
   // The array of additional buttons.
   // These buttons align to the right of the submit button.
-  const actionButtons = [
-    <Button text="Go back" onClick={goBack} />,
-    <Button text="Cancel" onClick={cancel} />,
-  ];
   return (
     <Fragment>
-      <Button
-        text={`Your size is. Click to change.`}
-        onClick={() => setOpen(true)}
-      />
+        <Button
+          text={`Choose Template`}
+          onClick={() => setOpen(true)}
+        />
+
+
       {isOpen && (
-        <ModalDialog header="My modal dialog" onClose={() => setOpen(false)}>
-      <Form onSubmit={onSubmit} actionButtons={actionButtons}>
-        <Text content="Description:" />
-        <TextField
-          name="description"
-          placeholder="Description:"
-          isRequired
-        />
-        <Text content="Expected:" />
-        <TextField
-          name="expected"
-          placeholder="Expected:"
-          isRequired
-        />
-        <Text content="Steps to replicate:" />
-        <TextField
-          name="steps"
-          placeholder="Steps to replicate:"
-          isRequired
-        />
-        <Text content="Status:" />
-        <CheckboxGroup name="status" label="Status">
-          <Checkbox value="Pass" label="Pass" />
-          <Checkbox value="Fail" label="Fail" />
-        </CheckboxGroup>
-        <Text content="Platforms:" />
-        <CheckboxGroup name="platforms" label="Platforms">
-          <Checkbox value="Android13" label="Android 13" />
-          <Checkbox value="Android11" label="Android 11" />
-          <Checkbox value="iOS17" label="iOS 17" />
-          <Checkbox value="iOS16" label="iOS 16" />
-        </CheckboxGroup>
-        <Text content="App Version:" />
-        <Select name="appVersion" isRequired>
-          <Option label="3.51" value="3.51" />
-          <Option label="3.50" value="3.50" />
-          <Option label="3.49" value="3.49" />
-        </Select>
-        <Text content="Cloud Version:" />
-        <Select name="cloudVersion" isRequired>
-          <Option label="Production" value="Production" />
-          <Option label="Development" value="Development" />
-        </Select>
-        <Button text="Submit" appearance="primary" />
-      </Form></ModalDialog>
+        <ModalDialog header="Bug Retest" onClose={() => setOpen(false)}>
+          <Form onSubmit={onSubmit}>
+{/*             <Text content="Description:" />
+            <TextField
+              name="description"
+              placeholder="Description:"
+              isRequired
+            />
+            <Text content="Expected:" />
+            <TextField
+              name="expected"
+              placeholder="Expected:"
+              isRequired
+            />
+            <Text content="Steps to replicate:" />
+            <TextField
+              name="steps"
+              placeholder="Steps to replicate:"
+              isRequired
+            /> */}
+     
+            <RadioGroup name="status" label="Status">
+              <Radio value="Pass" label="Pass" />
+              <Radio value="Fail" label="Fail" />
+            </RadioGroup>
+   
+            <CheckboxGroup name="platforms" label="Platforms">
+              <Checkbox value="Android13" label="Android 13" />
+              <Checkbox value="Android11" label="Android 11" />
+              <Checkbox value="iOS17" label="iOS 17" />
+              <Checkbox value="iOS16" label="iOS 16" />
+            </CheckboxGroup>
+    
+            <Select name="appVersion" isRequired>
+              <Option label="3.51" value="3.51" />
+              <Option label="3.50" value="3.50" />
+              <Option label="3.49" value="3.49" />
+            </Select>
+            <Text content="Cloud Version:" />
+            <Select name="cloudVersion" isRequired>
+              <Option label="Production" value="Production" />
+              <Option label="Development" value="Development" />
+            </Select>
+     
+          </Form></ModalDialog>
       )}
       {formState && <Text>{JSON.stringify(formState)}</Text>}
     </Fragment>
