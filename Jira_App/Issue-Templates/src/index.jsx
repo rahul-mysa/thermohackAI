42bdbde94b55249b8505b1661a76f6ecf23a61db
rahul-mysa <119628977+rahul-mysa@users.noreply.github.com>
2023-10-25 17:01:07 -0230

    last working code after hackathon

--- Jira_App/Issue-Templates/src/index.jsx
+++ Jira_App/Issue-Templates/src/index.jsx
@@ -2,12 +2,6 @@ import ForgeUI, {
   render,
   Form,
   Fragment,
-  TextField,
-  CheckboxGroup,
-  Checkbox,
-  Select,
-  Option,
-  Macro,
   useState,
   Text,
   IssuePanel,
@@ -15,70 +9,42 @@ import ForgeUI, {
   ModalDialog,
   RadioGroup,
   Radio,
-  Image
+  CheckboxGroup,
+  Checkbox,
+  Select,
+  Option,
+  // TextField,  // Commented out the unused components
+  // Image,
 } from "@forge/ui";
 
 const App = () => {
-  // useState is a UI kit hook we use to manage the form data in local state
   const [formState, setFormState] = useState(undefined);
   const [isOpen, setOpen] = useState(false);
 
-  // Handles form submission, which is a good place to call APIs, or to set component state...
   const onSubmit = async (formData) => {
-    /**
-     * formData:
-     * {
-     *    username: 'Username',
-     *    products: ['jira']
-     * }
-     */
     setFormState(formData);
   };
 
-  // The array of additional buttons.
-  // These buttons align to the right of the submit button.
   return (
     <Fragment>
-        <Button
-          text={`Choose Template`}
-          onClick={() => setOpen(true)}
-        />
-
+      <Button
+        text={`Choose Template`}
+        onClick={() => setOpen(true)}
+      />
 
       {isOpen && (
         <ModalDialog header="Bug Retest" onClose={() => setOpen(false)}>
           <Form onSubmit={onSubmit}>
-{/*             <Text content="Description:" />
-            <TextField
-              name="description"
-              placeholder="Description:"
-              isRequired
-            />
-            <Text content="Expected:" />
-            <TextField
-              name="expected"
-              placeholder="Expected:"
-              isRequired
-            />
-            <Text content="Steps to replicate:" />
-            <TextField
-              name="steps"
-              placeholder="Steps to replicate:"
-              isRequired
-            /> */}
-     
             <RadioGroup name="status" label="Status">
               <Radio value="Pass" label="Pass" />
               <Radio value="Fail" label="Fail" />
             </RadioGroup>
-   
             <CheckboxGroup name="platforms" label="Platforms">
               <Checkbox value="Android13" label="Android 13" />
               <Checkbox value="Android11" label="Android 11" />
               <Checkbox value="iOS17" label="iOS 17" />
               <Checkbox value="iOS16" label="iOS 16" />
             </CheckboxGroup>
-    
             <Select name="appVersion" isRequired>
               <Option label="3.51" value="3.51" />
               <Option label="3.50" value="3.50" />
@@ -89,8 +55,8 @@ const App = () => {
               <Option label="Production" value="Production" />
               <Option label="Development" value="Development" />
             </Select>
-     
-          </Form></ModalDialog>
+          </Form>
+        </ModalDialog>
       )}
       {formState && <Text>{JSON.stringify(formState)}</Text>}
     </Fragment>
